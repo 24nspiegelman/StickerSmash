0b4d2ae6d5572f49560a9a5672d00a28d56fc3af
24nspiegelman <24nspiegelman@athenian.org>
2024-02-11 10:47:21 -0800

    Working on Buttons

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const PlaceholderImage = require('/Users/Noah/StickerSmash/assets/background-image.png')
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer} >
+        <Image source={PlaceholderImage} style={style.image} />
+        </View>
+      <Text style={{color: '#fff'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
