1cdf4a9d9d95118096b4117b4f457d9fc7e8f6c9
24nspiegelman <24nspiegelman@athenian.org>
2024-02-13 09:40:45 -0800

    Incomplete

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 
-const PlaceholderImage = require('/Users/Noah/StickerSmash/assets/background-image.png')
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer} >
-        <Image source={PlaceholderImage} style={style.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
         </View>
-      <Text style={{color: '#fff'}}>Open up App.js to start working on your app!</Text>
+      <View style={styles.footerContainer}>
+        <Button /*theme="primary"*/ label="Choose a photo" />
+        <Button label="Use this photo" />
+        </View> 
       <StatusBar style="auto" />
     </View>
   );
@@ -20,6 +26,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer:{
+    flex: 1,
+    paddingTop: 58,
+  },
+image: {
+  width: 320,
+  height: 440,
+  borderRadius: 18,
+}
 });
